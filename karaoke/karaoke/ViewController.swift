e9c8f328e40904030ef245b2f07b52137cf5d474
Danny <danny@vinova.sg>
2015-12-15 23:47:49 +0700

    first commit

---
+++ karaoke/karaoke/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  karaoke
+//
+//  Created by Danny Le on 15/12/15.
+//  Copyright © 2015 com.danny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
